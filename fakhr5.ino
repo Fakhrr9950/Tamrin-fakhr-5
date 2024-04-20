7478a3c9f7b73fdbab5ae631950d7f5e1b42a3e5
Fakhrr9950 <166295478+Fakhrr9950@users.noreply.github.com>
2024-04-20 10:56:09 +0330

    Add files via upload

---
+++ fakhr5.ino
@@ -0,0 +1,22 @@
+
+int ledpin=3;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(ledpin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+float voltage=analogRead(A0);
+float voltage1=voltage/1023*5;
+Serial.print("Voltage=");
+Serial.println(voltage1);
+//delay(1000);
+float voltage2=map(voltage,0,1023,0,255);
+analogWrite(3,voltage2);
+
+
+
+
+}
